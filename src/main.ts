f9cef368c5a529c0930e72c926e35ad31eeaa855
敖律风 <x123456789fy@outlook.com>
2025-01-31 17:31:03 +0800

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import 'virtual:uno.css'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(ElementPlus)
+    // .use(router)
+    .mount('#app')
+if (import.meta.env.MODE === "development") {
+
+}
