2c303b6428158c7d898b81fff98aeb0098ca3bc1
敖律风 <x123456789fy@outlook.com>
2025-01-31 22:50:53 +0800

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import {defineConfig} from 'vite'
 import UnoCSS from 'unocss/vite'
 import vue from '@vitejs/plugin-vue'
@@ -5,4 +6,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [vue(), UnoCSS()],
+    test:{}
 })
